4f9249bffabb899f9a52034f36bf85fc1c9d1eb3
yuzai <958684803@qq.com>
2025-02-28 14:23:31 +0800

    初始化项目

---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div ref="editorContainer" id="editor"></div>
+  </div>
+</template>
+<script setup lang="ts">
+import EditorJS from '@editorjs/editorjs';
+import { onMounted, ref } from 'vue';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+
+const editorContainer = ref<HTMLDivElement | null>(null);
+const editor = ref<EditorJS | null>(null);
+onMounted(() => {
+  editor.value = new EditorJS({
+    holder: 'editor',
+    placeholder: '开始编辑内容',
+    tools: {
+      header: Header,
+      list: List
+    }
+  })  
+});
+
+</script>
+
+<style>
+#editor {
+  width: 100%;
+  min-height: 500px;
+  margin-top: 20px;
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
